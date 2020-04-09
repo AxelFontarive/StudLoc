be5b8dd5c7b672cd9c212d94a3227327cbd43cc0
Axel Fontarive <afontarive@gmail.com>
2020-04-09 09:58:52 +0200

    Ajout de fonctionnalités dans les classes utilitaires.

--- src/js/utils.js
+++ src/js/utils.js
@@ -14,12 +14,24 @@ export class DbUtils {
     }
 
     /**
-     * Retourne un objet JSON contenant le premier utilisateur correspondant à l'identificateur unique `uid`.
+     * Retourne un objet JSON contenant l'utilisateur correspondant à son identificateur d'authentification unique `uid`.
+     * NB : Le champ recherché est `AuthUID` et non pas `ID`.
      * @param {string} uid Identificateur unique de l'utilisateur.
-     * @example await getUserByUid("Cc0YbyxXPSckFWjXcj2bduNwAVS2");
+     * @example await getUserByAuthUid("Cc0YbyxXPSckFWjXcj2bduNwAVS2");
      */
-    static async getUserByUid(uid) {
-        let snapshot = await db.collection("User").where("ID", "==", uid).get();
+    static async getUserByAuthUid(uid) {
+        let snapshot = await db.collection("User").where("AuthUID", "==", uid).get();
+        return snapshot.docs[0].data();
+    }
+
+    /**
+     * Retourne un objet JSON contenant le premier document dans la collection `collection` dont l'identificateur correspond à `id`.
+     * @param {string} collection Collection cible
+     * @param {string} id Identificateur unique du document
+     * @example var usr = await DbUtils.getDocumentById("User", "deb7df071");
+     */
+    static async getDocumentById(collection, id) {
+        let snapshot = await db.collection(collection).where("ID", "==", id).get();
         return snapshot.docs[0].data();
     }
 
@@ -46,6 +58,12 @@ export class DbUtils {
         return snapshot.docs.map(d => d.data());
     }
     
+    /**
+     * Retourne un `string` contenant un identificateur quasi-unique (nombre formaté en hexadécimal).
+     */
+    static generatePseudoRandomId() {
+        return Math.floor(Date.now() * Math.random()).toString(16);
+    }
 }
 
 /**
@@ -53,4 +71,12 @@ export class DbUtils {
  */
 export class CodeUtils {
 
+    /**
+     * Formate le Timestamp Firestore en date courte (dd/MM/yyyy).
+     * @param {firebase.firestore.Timestamp} firestoreDate Timestamp Firestore à convertir.
+     */
+    static toShortDate(firestoreDate) {
+        var dfb = firestoreDate.toDate();
+        return `${dfb.getFullYear()}-${dfb.getMonth() + 1}-${dfb.getDate()}`;
+    }
 }
