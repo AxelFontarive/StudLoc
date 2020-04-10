72e4c29eb13f504b93e15e87fd88b273a9eec1fc
Elliot <elliot.pujol@etu.univ-smb.fr>
2020-04-10 08:58:05 +0200

    Profil ajout

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import '../css/profil.css';
+import IconeProfil from '../images/IconeProfil.png';
 
 function Profil(){
     return(
         <div>
             <h1>Quentin bois plus et Guillaume fume mieux</h1>
-            <img class="fit-picture" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTYZWKOl3X6dgmU4J64gpnBPqk9p-14y9Pvkzqop8eZaFkHPmkQ&usqp=CAU" alt="Image de profil non trouvé"></img>
+            <img class="fit-picture" src={IconeProfil} alt="Image de profil non trouvé"></img>
             <section>
                 <label>NOM PRENOM - Age</label>
             </section>
             <section class="rating rating2">
-                <a href="#5" title="Give 5 stars">★</a>
-                <a href="#4" title="Give 4 stars">★</a>
-                <a href="#3" title="Give 3 stars">★</a>
-                <a href="#2" title="Give 2 stars">★</a>
-                <a href="#1" title="Give 1 star">★</a>
+                <a href="#5" title="Donner 5 étoiles">★</a>
+                <a href="#4" title="Donner 4 étoiles">★</a>
+                <a href="#3" title="Donner 3 étoiles">★</a>
+                <a href="#2" title="Donner 2 étoiles">★</a>
+                <a href="#1" title="Donner 1 étoiles">★</a>
             </section>
         </div>
     )
