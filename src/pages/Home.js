fe5bac17a1afaeeab82aa590e2c231acb59e9286
Axel Fontarive <afontarive@gmail.com>
2020-06-24 09:33:37 +0200

    Ajout des liens depuis page d'accueil + centrage image 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import {
+BrowserRouter as Router,
+Switch,
+Route,
+Link
+} from "react-router-dom";
 import '../css/home.css';
 
 
@@ -10,15 +16,15 @@ function Home(){
                 <h1 id="TitreHome">Trouvez dès maintenant le logement qui vous correspond</h1>
                 <div id="gauche" class="BordureDiv">
                     <div id="ImageHome1"></div>
-                    <p class="SousTitre">Voir liste des logements</p>
+                    <p class="SousTitre"><Link to="/logements">Liste de logements</Link></p>
                 </div>
                 <div id="centre" class="BordureDiv">
                     <div id="ImageHome2"></div>
-                    <p class="SousTitre">Recherchez un logement</p>
+                    <p class="SousTitre"><Link to="/recherche">Recherchez un logement</Link></p>
                 </div>
                 <div id="droite" class="BordureDiv">
                     <div id="ImageHome3"></div>
-                    <p class="SousTitre">Suivez vos réservations</p>
+                    <p class="SousTitre"><Link to="/profil">Suivez vos réservations</Link></p>
                  </div>
             </section>
         </div>
